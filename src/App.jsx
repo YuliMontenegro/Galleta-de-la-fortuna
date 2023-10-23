f7c651344dc42e5363da01b610e408f4116e9c41
Yuli <yupamova20@gmail.com>
2023-10-23 15:44:31 -0500

    avances

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+import phrases from './utils/phrases.json'
+import bgArr from './utils/bgArr.json'
+import getRandomFromArr from './services/getRandomFromArr'
+import Phrase from './components/Phrase'
+import ButtonPhrase from './components/ButtonPhrase'
+
+
+function App() {
+
+  const quote = getRandomFromArr(phrases)
+  
+  const [phraseRandom, setPhraseRandom] = useState(quote)
+  const [bgApp, setBgApp] = useState(getRandomFromArr(bgArr))
+
+  const objStyle = {
+    backgroundImage: `url('../fondo${bgApp}.jpg')`
+  }
+
+  return (
+      <div style={objStyle} className='app'>
+        <h1 className='app__tittle'>Galleta de la fortuna</h1>
+        <article className='app__card'>
+        <Phrase
+          phraseRandom={phraseRandom}
+        />
+        <ButtonPhrase
+          setPhraseRandom={setPhraseRandom}
+          setBgApp={setBgApp}
+        />
+        </article>
+      </div>
+  )
+}
+
+export default App
